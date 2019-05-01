5f52cb5a81a2f9b2047ff53a0ee364adf823e19a
JulianStange <49147127+JulianStange@users.noreply.github.com>
2019-05-01 15:39:27 +0200

    Delete de.js

--- l10n/de.js
+++
@@ -1,9 +0,0 @@
-OC.L10N.register(
-    "extract",
-    {
-    "Extract here" : "Hier Entpacken",
-    "Error extracting " : "Fehlen beim entpacken",
-    "Extract" : "Enpacke",
-    "Extract archive from the web interface" : "Enpacke Archive aus dem Web Interface"
-},
-"");

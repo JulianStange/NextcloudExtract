98cd07c30a4e464255dde98fa7b6cd4e58308f10
JulianStange <49147127+JulianStange@users.noreply.github.com>
2019-05-01 15:36:14 +0200

    Add files via upload
    Add l10n German

---
+++ l10n/de.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "extract",
+    {
+    "Extract here" : "Hier Entpacken",
+    "Error extracting " : "Fehlen beim entpacken",
+    "Extract" : "Enpacke",
+    "Extract archive from the web interface" : "Enpacke Archive aus dem Web Interface"
+},
+"");
